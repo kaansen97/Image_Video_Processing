244c6879b507b9ad5ab69fa4cee1e955a9bf5c9f
kaansen97 <kaan-egemen-sen@hotmail.com>
2023-04-07 19:09:19 +0200

    Create Combine_linear_op
    Might be answer to Q2

---
+++ Combine_Linear.m
@@ -0,0 +1,14 @@
+im_arch = imread("delicate_arch.jpg");
+im_arch = double(im_arch)./255;
+
+% im_arch = histeq(im_arch,10);
+im_arch_blur = imgaussfilt(im_arch,3);
+
+subplot(1,2,1);
+imshow(im_arch);
+title('Input image')
+
+subplot(1,2,2);
+Sharp=im_arch+(im_arch-im_arch_blur);
+imshow(Sharp);
+title('Image Sharpened with sigma = 3')
